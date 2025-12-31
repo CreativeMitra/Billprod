27e6c0822d8bfc4356acd17c1aca79ea55b8a75c
CreativeMitra <upkartdailyfinds@gmail.com>
2025-12-31 21:54:58 +0530

    Add files via upload

---
+++ src/lib/supabase.ts
@@ -0,0 +1,6 @@
+import { createClient } from "@supabase/supabase-js";
+
+export const supabase = createClient(
+  "https://viaxspuyhtygzmjclxnx.supabase.co",
+  "sb_publishable__TEr2hYBnjWAI5uPWBkJ6w_M_BP_L4z"
+);
